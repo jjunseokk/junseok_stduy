35adb93c58f2478f9082197329bdabf5fc030006
junseok park <p.junseok53@gmail.com>
2022-10-09 23:01:06 +0900

    save

--- 01. Study/10. JS project/04. Final Project/main.js
+++ 01. Study/10. JS project/04. Final Project/main.js
@@ -135,7 +135,7 @@ let url;
 // api 호출 함수를 부른다.
 const getNews = async () => {
     try {
-        let header = new Headers({ 'x-api-key': 'YyWPEejOB78qp7EclzhMNYfaSo2mov-RlyVF_MTcZMU' });
+        let header = new Headers({ 'x-api-key': 'HpMrmb5OOah-65i0d7hVNMpLNdy6qobbzTtFkp5BKR4' });
 
         url.searchParams.set('page', page); //page 움직일때 키를 준다.
         let response = await fetch(url, { headers: header }); //ajax, http, fetch /*await은 이 일이 끝날때까지 기다려라 라는 뜻.*/
